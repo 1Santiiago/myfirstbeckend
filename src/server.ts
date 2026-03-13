1913ad236113b5019813b18b35898e25eb6ffba9
Santiago Ferreira <santiagoferreirati@gmail.com>
2026-03-13 09:50:49 -0300

    name swagger

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ const swaggerOptions = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "API Loja",
+      title: "My First Backend",
       version: "1.0.0",
       description: "Documentação da API"
     },
